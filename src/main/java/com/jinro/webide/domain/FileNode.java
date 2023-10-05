4c656edc843bd821cea5281446d7ee3b26fc1535
jeosong <13bitadc@naver.com>
2023-10-05 16:34:32 +0900

    style: json key 이름을 open -> isOpen으로 변경

--- src/main/java/com/jinro/webide/domain/FileNode.java
+++ src/main/java/com/jinro/webide/domain/FileNode.java
@@ -1,5 +1,6 @@
 package com.jinro.webide.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,5 +13,7 @@ public class FileNode {
     private List<FileNode> children;
     private int _id;
     private int checked = 0;
+
+    @JsonProperty("isOpen") // 명시적으로 JSON 키 이름 지정
     private boolean isOpen = false;
 }
