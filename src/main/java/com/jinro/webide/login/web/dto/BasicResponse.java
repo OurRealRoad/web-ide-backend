0698619ce158c71b8e6289ebd3800bebba040735
ydmins <dr.ydmins@gmail.com>
2023-10-09 17:10:42 +0900

    refactor: add email check api

---
+++ src/main/java/com/jinro/webide/login/web/dto/BasicResponse.java
@@ -0,0 +1,15 @@
+package com.jinro.webide.login.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasicResponse {
+    private boolean result;
+    private String message;
+}
