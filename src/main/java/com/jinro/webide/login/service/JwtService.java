9282ad8690622b13898d7a9ff239ccb6455a9ca2
ydmins <dr.ydmins@gmail.com>
2023-10-09 20:55:12 +0900

    refactor: add response object at exception cuased by an expired jwt token

--- src/main/java/com/jinro/webide/login/service/JwtService.java
+++ src/main/java/com/jinro/webide/login/service/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
                 .claim("name", name)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 1))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
